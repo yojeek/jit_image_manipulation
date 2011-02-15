39cc1ea3e5d09b2a8a16c54431b8917984198450
Jonas Coch <klaftertief@googlemail.com>
2011-02-15 17:02:01 +0100

    preferences for named rules

--- extension.driver.php
+++ extension.driver.php
@@ -36,24 +36,114 @@
 		}		
 		
 		public function __SavePreferences($context){
+			$context['settings']['image']['named_rules'] = serialize($context['settings']['image']['named_rules']);
 			$this->saveTrusted(stripslashes($_POST['jit_image_manipulation']['trusted_external_sites']));
 		}
 		
 		public function appendPreferences($context){
 
-			$group = new XMLElement('fieldset');
-			$group->setAttribute('class', 'settings');
-			$group->appendChild(new XMLElement('legend', __('JIT Image Manipulation')));			
+			$fieldset = new XMLElement('fieldset');
+			$fieldset->setAttribute('class', 'settings');
+			$fieldset->appendChild(new XMLElement('legend', __('JIT Image Manipulation')));
 			
 			$label = Widget::Label(__('Trusted Sites'));
 			$label->appendChild(Widget::Textarea('jit_image_manipulation[trusted_external_sites]', 10, 50, $this->trusted()));
 			
+			$fieldset->appendChild($label);
+			
+			$fieldset->appendChild(new XMLElement('p', __('Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.'), array('class' => 'help')));
+			
+			// named rules
+			$named_rules = unserialize(Symphony::Configuration()->get('named_rules', 'image'));
+			$positions = array(
+				__('Left top'),
+				__('Center top'),
+				__('Right top'),
+				__('Left center'),
+				__('Center'),
+				__('Right center'),
+				__('Left bottom'),
+				__('Center bottom'),
+				__('Right bottom'),
+			);
+			
+			$subsection = new XMLElement('div');
+			$subsection->setAttribute('class', 'subsection');
+			$subsection->appendChild(new XMLElement('h3', __('Named Rules'), array('class' => 'label')));
+ 
+			$ol = new XMLElement('ol');
+			$li = new XMLElement('li');
+			$li->setAttribute('class', 'template');
+			$li->appendChild(new XMLElement('h4', __('Rule')));
+
+			$group = new XMLElement('div');
+			$group->setAttribute('class', 'group');
+			$label = Widget::Label(__('Name'));
+			$label->appendChild(Widget::Input('settings[image][named_rules][-1][name]'));
 			$group->appendChild($label);
-						
-			$group->appendChild(new XMLElement('p', __('Leave empty to disable external linking. Single rule per line. Add * at end for wild card matching.'), array('class' => 'help')));
-									
-			$context['wrapper']->appendChild($group);
-						
+			$label = Widget::Label(__('URL parameters'));
+			$label->appendChild(Widget::Input('settings[image][named_rules][-1][url-parameters]'));
+			$group->appendChild($label);
+			$li->appendChild($group);
+
+			$group = new XMLElement('div');
+			$group->setAttribute('class', 'group');
+			$label = Widget::Label(__('Watermark'));
+			$label->appendChild(Widget::Input('settings[image][named_rules][-1][watermark]'));
+			$group->appendChild($label);
+			$label = Widget::Label(__('Watermark position'));
+			$options = array();
+			for ($i = 1; $i <= 9; $i++) { 
+				$options[$i] = array($i);
+			}
+			$label->appendChild(Widget::Select('settings[image][named_rules][-1][watermark-position]', $options));
+			$group->appendChild($label);
+			$li->appendChild($group);
+
+			$ol->appendChild($li);
+
+			if(is_array($named_rules)) {
+				$i = 1;
+				foreach($named_rules as $rule) {
+					$li = new XMLElement('li');
+					$li->setAttribute('class', 'instance expanded');
+					$li->appendChild(new XMLElement('h4', __('Rule')));
+
+					$group = new XMLElement('div');
+					$group->setAttribute('class', 'group');
+					$label = Widget::Label(__('Name'));
+					$label->appendChild(Widget::Input("settings[image][named_rules][{$i}][name]", $rule['name']));
+					$group->appendChild($label);
+					$label = Widget::Label(__('URL parameters'));
+					$label->appendChild(Widget::Input("settings[image][named_rules][{$i}][url-parameters]", $rule['url-parameters']));
+					$group->appendChild($label);
+					$li->appendChild($group);
+
+					$group = new XMLElement('div');
+					$group->setAttribute('class', 'group');
+					$label = Widget::Label(__('Watermark'));
+					$label->appendChild(Widget::Input("settings[image][named_rules][{$i}][watermark]", $rule['watermark']));
+					$group->appendChild($label);
+					$label = Widget::Label(__('Watermark position'));
+					$options = array();
+					for ($i = 1; $i <= 9; $i++) { 
+						$options[$i] = array($i, ($i == $rule['watermark-position']), $positions[$i - 1]);
+					}
+					$label->appendChild(Widget::Select("settings[image][named_rules][{$i}][watermark-position]", $options));
+					$group->appendChild($label);
+					$li->appendChild($group);
+
+					$ol->appendChild($li);
+					$i++;
+				}
+			}
+
+			$subsection->appendChild($ol);
+			
+			$fieldset->appendChild($subsection);
+			
+			$context['wrapper']->appendChild($fieldset);
+			
 		}
 		
 		public function enable(){
